a91ec01e54b395d59c4fed4594e59e21d6760cac
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-06-26 01:22:06 +0300

    [GA] Add investigator strategy

--- ai-challenge-ants/cpp_starter_package/Bot.cc
+++ ai-challenge-ants/cpp_starter_package/Bot.cc
@@ -1,3 +1,5 @@
+#include <map>
+
 #include "Bot.h"
 
 using namespace std;
@@ -30,19 +32,47 @@ void Bot::makeMoves()
 {
     state.bug << "turn " << state.turn << ":" << endl;
     state.bug << state << endl;
+    static auto weights = vector<vector<double>>(state.rows, vector<double>(state.cols, 0.0));
+    for(auto& row: weights)
+    {
+        for(auto& column: row)
+        {
+            column *= 0.9;
+        }
+    }
 
     //picks out moves for each ant
     for(int ant=0; ant<(int)state.myAnts.size(); ant++)
     {
+        int direction;
+        Location target;
+        std::map<int, Location> directions;
         for(int d=0; d<TDIRECTIONS; d++)
         {
             Location loc = state.getLocation(state.myAnts[ant], d);
 
             if(!state.grid[loc.row][loc.col].isWater)
             {
-                state.makeMove(state.myAnts[ant], d);
-                break;
+                direction = d;
+                target = loc;
+
+                directions[d] = loc;
             }
+        }
+
+        for(const auto& kv: directions)
+        {
+            if(weights[kv.second.row][kv.second.col] < weights[target.row][target.col])
+            {
+                direction = kv.first;
+                target = kv.second;
+            }
+        }
+
+        if (directions.size() > 0)
+        {
+            weights[target.row][target.col] += 0.1;
+            state.makeMove(state.myAnts[ant], direction);
         }
     }
 
