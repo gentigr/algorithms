ec789594412008d14dc0df4ef55ae2a021c494b4
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-06-26 15:37:21 +0300

    [GA] Add simple hunter strategy based on attack map and enemy hills

--- ai-challenge-ants/cpp_starter_package/Bot.cc
+++ ai-challenge-ants/cpp_starter_package/Bot.cc
@@ -20,111 +20,179 @@ void Bot::playGame()
     endTurn();
 
     //continues making moves while the game is not over
-    while(cin >> state)
-    {
+    while (cin >> state) {
         state.updateVisionInformation();
         makeMoves();
         endTurn();
     }
 };
 
-bool Bot::investigator(int size)
-{
-    static bool investigator = false;
-    if (size > 1)
-    {
-        investigator = true;
-    }
-    return investigator || size > 1;
-}
-
-bool Bot::conservator(int row, int col, const std::vector<Location>& hills)
-{
-    for(const auto& hill: hills)
-    {
-        if (sqrt(pow(row - hill.row, 2) + pow(col - hill.col, 2)) < 5)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 void Bot::normalize(std::vector<std::vector<double>>& weights)
 {
     int max = INT_MIN;
-    for(auto& row: weights)
-    {
-        for(auto& column: row)
-        {
-            if (column > max)
-            {
+    for (auto& row : weights) {
+        for (auto& column : row) {
+            if (column > max) {
                 max = column;
             }
         }
     }
-    for(auto& row: weights)
-    {
-        for(auto& column: row)
-        {
+    for (auto& row : weights) {
+        for (auto& column : row) {
             column /= max;
         }
     }
-}
-
-void print(const std::vector<std::vector<double>>& weights)
-{
-    std::fstream f;
-    f.open("/tmp/mtx.txt", std::fstream::in | std::fstream::app | std::fstream::out);
-    for(int i = 0; i < weights.size(); i++)
-    {
-        for(int j = 0; j < weights[0].size(); j++)
-        {
-            f << weights[i][j] << ' ';
-        }
-        f << std::endl;
-        f.flush();
-    }
-    f << std::endl;
-    f.close();
 }
 
 void Bot::updateMap(std::vector<std::vector<double>>& weights,
                     const std::vector<Location>& hills,
                     int size)
 {
-    for(auto& row: weights)
-    {
-        for(auto& column: row)
-        {
-            column *= 0.9;
+    for (auto& row : weights) {
+        for (auto& column : row) {
+            column *= 0.95;
         }
     }
-    if (size == 1) // conservator
-    {
-        double mtx[weights.size()][weights[0].size()]{};
-        for(const auto& hill: hills)
-        {
-            for(int i = 0; i < weights.size(); i++)
-            {
-                for(int j = 0; j < weights[0].size(); j++)
-                {
-                    mtx[i][j] += 0.1*(abs(i - hill.row) + abs(j - hill.col)) / hills.size();
+}
+
+void Bot::homester(State& st,
+                   std::vector<std::vector<double>>& weights,
+                   const Location& ant,
+                   const Location& hill)
+{
+    int direction;
+    Location target;
+    std::map<int, Location> directions;
+    for (int d = 0; d < TDIRECTIONS; d++) {
+        Location loc = st.getLocation(ant, d);
+
+        if (!st.grid[loc.row][loc.col].isWater) {
+            directions[d] = loc;
+            direction = d;
+            target = loc;
+        }
+    }
+
+    std::map<int, Location> inborders;
+    for (const auto& kv : directions) {
+        if (sqrt(pow(kv.second.row - hill.row, 2) + pow(kv.second.col - hill.col, 2)) < 6) {
+            inborders[kv.first] = kv.second;
+        }
+    }
+
+    if (inborders.size() == 0) {
+        double euklid = LONG_MAX;
+        for (const auto& kv : directions) {
+            double length = sqrt(pow(kv.second.row - hill.row, 2) + pow(kv.second.col - hill.col, 2));
+            if (euklid > length) {
+                euklid = length;
+                direction = kv.first;
+                target = kv.second;
+            }
+        }
+    }
+
+    for (const auto& kv : inborders) {
+        if (weights[kv.second.row][kv.second.col] < weights[target.row][target.col]) {
+            direction = kv.first;
+            target = kv.second;
+        }
+    }
+
+    weights[target.row][target.col] += 0.1;
+    st.makeMove(ant, direction);
+}
+
+void Bot::scout(State& st,
+                std::vector<std::vector<double>>& weights,
+                const Location& ant)
+{
+    int direction;
+    Location target;
+    std::map<int, Location> directions;
+    for (int d = 0; d < TDIRECTIONS; d++) {
+        Location loc = st.getLocation(ant, d);
+
+        if (!st.grid[loc.row][loc.col].isWater) {
+            directions[d] = loc;
+            direction = d;
+            target = loc;
+        }
+    }
+
+    for (const auto& kv : directions) {
+        if (weights[kv.second.row][kv.second.col] < weights[target.row][target.col]) {
+            direction = kv.first;
+            target = kv.second;
+        }
+    }
+
+    weights[target.row][target.col] += 0.1;
+    st.makeMove(ant, direction);
+}
+
+void Bot::hunter(State& st,
+                 std::vector<std::vector<double>>& weights,
+                 const Location& ant,
+                 const std::vector<Location> enemyHills)
+{
+    // TODO: remove hill from target hills vector when achieve it
+    int direction;
+    Location target;
+    std::map<int, Location> directions;
+    static std::vector<Location> targetHills;
+    static auto attacks = vector<vector<double>>(state.rows, vector<double>(state.cols, 0.0));
+
+    for (int d = 0; d < TDIRECTIONS; d++) {
+        Location loc = st.getLocation(ant, d);
+
+        if (!st.grid[loc.row][loc.col].isWater) {
+            directions[d] = loc;
+            direction = d;
+            target = loc;
+        }
+    }
+
+    if (enemyHills.size() > 0) {
+        for (const auto& hill : enemyHills) {
+            for (const auto& target : targetHills) {
+                if (target.row == hill.row && target.col == hill.col) {
+                    break;
+                }
+                targetHills.push_back(hill);
+            }
+        }
+    }
+
+    if (targetHills.size() > 0) {
+        for (const auto& hill : targetHills) {
+            for (int i = 0; i < weights.size(); i++) {
+                for (int j = 0; j < weights[0].size(); j++) {
+                    attacks[i][j] += 0.1 * (abs(i - hill.row) + abs(j - hill.col)) / targetHills.size();
                 }
             }
         }
-        for(int i = 0; i < weights.size(); i++)
-        {
-            for(int j = 0; j < weights[0].size(); j++)
-            {
-                weights[i][j] += mtx[i][j];
+        this->normalize(attacks);
+
+        for (const auto& kv : directions) {
+            double suggested = weights[kv.second.row][kv.second.col] + attacks[kv.second.row][kv.second.col];
+            double previous = weights[target.row][target.col] + attacks[target.row][target.col];
+            if (suggested < previous) {
+                direction = kv.first;
+                target = kv.second;
+            }
+        }
+    } else {
+        for (const auto& kv : directions) {
+            if (weights[kv.second.row][kv.second.col] < weights[target.row][target.col]) {
+                direction = kv.first;
+                target = kv.second;
             }
         }
     }
-    else if (size > 10) // hunter
-    {
-    }
+
+    weights[target.row][target.col] += 0.1;
+    st.makeMove(ant, direction);
 }
 
 //makes the bots moves for the turn
@@ -133,71 +201,28 @@ void Bot::makeMoves()
     state.bug << "turn " << state.turn << ":" << endl;
     state.bug << state << endl;
     static auto weights = vector<vector<double>>(state.rows, vector<double>(state.cols, 0.0));
-#if 0
-    print(weights);
     this->updateMap(weights, state.myHills, state.myAnts.size());
-#endif
 
     //picks out moves for each ant
-    for(int ant=0; ant<(int)state.myAnts.size(); ant++)
-    {
-        if (ant == 0)
-        {
-            // homester
-        }
-        else if (ant > 0 && ant < 11)
-        {
-            // investigator
-        }
-        else
-        {
-            // hunter
-        }
-        int direction;
-        Location target;
-        std::map<int, Location> directions;
-        for(int d=0; d<TDIRECTIONS; d++)
-        {
-            Location loc = state.getLocation(state.myAnts[ant], d);
-
-            if(!state.grid[loc.row][loc.col].isWater
-                && (investigator(state.myAnts.size())
-                    || conservator(loc.row, loc.col, state.myHills)))
-            {
-                direction = d;
-                target = loc;
-
-                directions[d] = loc;
-            }
-        }
-
-        for(const auto& kv: directions)
-        {
-            if(weights[kv.second.row][kv.second.col] < weights[target.row][target.col])
-            {
-                direction = kv.first;
-                target = kv.second;
-            }
-        }
-
-        if (directions.size() > 0)
-        {
-            weights[target.row][target.col] += 0.1;
-            state.makeMove(state.myAnts[ant], direction);
+    for (int ant = 0; ant < (int)state.myAnts.size(); ant++) {
+        if (ant < state.myHills.size()) {
+            this->homester(state, weights, state.myAnts[ant], state.myHills[ant]);
+        } else if (ant >= state.myHills.size() && ant < 11) {
+            this->scout(state, weights, state.myAnts[ant]);
+        } else {
+            this->hunter(state, weights, state.myAnts[ant], state.enemyHills);
         }
     }
 
-#if 0
-    this->normalize(weights);
-#endif
     state.bug << "time taken: " << state.timer.getTime() << "ms" << endl << endl;
 };
 
 //finishes the turn
 void Bot::endTurn()
 {
-    if(state.turn > 0)
+    if (state.turn > 0) {
         state.reset();
+    }
     state.turn++;
 
     cout << "go" << endl;
--- ai-challenge-ants/cpp_starter_package/Bot.h
+++ ai-challenge-ants/cpp_starter_package/Bot.h
@@ -6,8 +6,7 @@
 /*
     This struct represents your bot in the game of Ants
 */
-struct Bot
-{
+struct Bot {
     State state;
 
     Bot();
@@ -17,11 +16,22 @@ struct Bot
     void makeMoves();   //makes moves for a single turn
     void endTurn();     //indicates to the engine that it has made its moves
 
-    bool investigator(int size);
-    bool conservator(int row, int col, const std::vector<Location>& hills);
-
-    void updateMap(std::vector<std::vector<double>>& weights, const std::vector<Location>& hills, int size);
+    void updateMap(std::vector<std::vector<double>>& weights,
+                   const std::vector<Location>& hills,
+                   int size);
     void normalize(std::vector<std::vector<double>>& weights);
+
+    void homester(State& state,
+                  std::vector<std::vector<double>>& weights,
+                  const Location& ant,
+                  const Location& hill);
+    void scout(State& state,
+               std::vector<std::vector<double>>& weights,
+               const Location& ant);
+    void hunter(State& state,
+                std::vector<std::vector<double>>& weights,
+                const Location& ant,
+                const std::vector<Location> enemyHills);
 };
 
 #endif //BOT_H_
