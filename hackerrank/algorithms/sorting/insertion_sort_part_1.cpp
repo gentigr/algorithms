a1e09a4194ad6c1abe34f956e99674d132cab370
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-07-21 18:36:56 +0300

    [HR][ALGO][SORT] Insertion Sort - Part 1

---
+++ hackerrank/algorithms/sorting/insertion_sort_part_1.cpp
@@ -0,0 +1,44 @@
+// https://www.hackerrank.com/challenges/insertionsort1
+#include <vector>
+#include <iostream>
+
+template<typename T>
+void print_collection(const std::vector<T>& data)
+{
+    for(const auto& item : data) {
+        std::cout << item << " ";
+    }
+    std::cout << std::endl;
+}
+
+template<typename T>
+void insertion_sort(std::vector<T>& input, T value)
+{
+    for(auto it = input.rbegin(); it != input.rend() - 1; ++it) {
+        *it = (*(it + 1) <= value) ? value : *(it + 1);
+        print_collection<T>(input);
+        if (*(it + 1) <= value) {
+            return;
+        }
+    }
+    // that case for first item if we are here
+    input[0] = value;
+    print_collection<T>(input);
+}
+
+int main()
+{
+    int size;
+    std::cin >> size;
+
+    std::vector<int> data;
+    for(int i = 0; i < size; ++i) {
+        int number;
+        std::cin >> number;
+        data.push_back(number);
+    }
+
+    insertion_sort(data, data.back());
+
+    return 0;
+}
