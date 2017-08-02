0099b295b3fa8382885b5c6d44308281c1c43758
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-02 16:34:06 +0300

    [HR][ALGO][SORT] Intro to Tutorial Challenges

---
+++ hackerrank/algorithms/sorting/intro_to_tutorial_challenges.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/tutorial-intro
+#include <iostream>
+
+// of course, it is possible to use binary search here,
+// but if we may not to read all data, why we must do that?
+int main()
+{
+    int value;
+    std::cin >> value;
+    int count;
+    std::cin >> count;
+
+    for(int i = 0; count-- != 0; ++i) {
+        int number;
+        std::cin >> number;
+        if (value == number) {
+            std::cout << i << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
