abe153a9273a151278372dfe56a66af480423a9d
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-10 15:49:57 +0300

    [HR][ALGO][BITM] Lonely Integer

---
+++ hackerrank/algorithms/bit_manipulation/lonely_integer.cpp
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/lonely-integer
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    int result = 0;
+    for(int number; n-- != 0 && std::cin >> number; result ^= number);
+
+    std::cout << result << std::endl;
+
+    return 0;
+}
