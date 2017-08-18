2b3e880b2793ef6b418580ef1bc59aebe268eefe
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-18 11:31:01 +0300

    [HR][ALGO][STR] CamelCase

---
+++ hackerrank/algorithms/strings/camelcase.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/camelcase
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    std::string line;
+    std::cin >> line;
+
+    int count = 0;
+    auto solver = [&count](char c) { count += (c > 64 && c < 91); };
+    std::for_each(line.begin(), line.end(), solver);
+
+    std::cout << (count + 1) << std::endl;
+
+    return 0;
+}
