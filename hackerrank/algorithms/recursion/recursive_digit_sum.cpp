521249d2fa2e34e835015e27ad2f58232b2dddc7
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-19 12:59:37 +0300

    [HR][ALGO][RECU] Recursive Digit Sum

---
+++ hackerrank/algorithms/recursion/recursive_digit_sum.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/recursive-digit-sum
+#include <iostream>
+#include <algorithm>
+
+long long determine_super_digit(std::string number)
+{
+    long long sum = 0;
+    std::for_each(number.cbegin(), number.cend(), [&sum](const char c) { sum += (c - 48); });
+    return sum > 9 ? determine_super_digit(std::to_string(sum)) : sum;
+}
+
+int main()
+{
+    std::string n;
+    std::cin >> n;
+    int k;
+    std::cin >> k;
+
+    long long result = determine_super_digit(n);
+    std::cout << determine_super_digit(std::to_string(result * k)) << std::endl;
+
+    return 0;
+}
