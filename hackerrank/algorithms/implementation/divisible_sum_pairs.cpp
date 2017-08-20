cdcdb8c9b8486fd47485f3bbbb5d2d96577618c8
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-20 22:34:05 +0300

    [HR][ALGO][IMPL] Divisible Sum Pairs

---
+++ hackerrank/algorithms/implementation/divisible_sum_pairs.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/divisible-sum-pairs
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<int> a;
+    for(int number; n-- > 0 && std::cin >> number; a.push_back(number));
+
+    int count = 0;
+    for(int i = 0; i < a.size() - 1; ++i) {
+        for(int j = i + 1; j < a.size(); ++j) {
+            count += ((a[i] + a[j]) % k == 0);
+        }
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
+}
