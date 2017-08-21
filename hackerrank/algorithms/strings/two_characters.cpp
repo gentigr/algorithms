30f531b8488f7168b3126b59d3ebe6293b725094
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-21 17:59:00 +0300

    [HR][ALGO][STR] Two Characters

---
+++ hackerrank/algorithms/strings/two_characters.cpp
@@ -0,0 +1,51 @@
+// https://www.hackerrank.com/challenges/two-characters
+#include <set>
+#include <iostream>
+#include <algorithm>
+
+int sequence_length(std::string& input, char first, char second)
+{
+    int count = 0;
+    char previous = '\0';
+    for(char ch : input) {
+        if (ch == first || ch == second) {
+            if (previous == ch) {
+                return -1;
+            }
+
+            previous = ch;
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+int max_sequence_length(std::string& input)
+{
+    std::set<char> unique_letters;
+    for(char c : input) {
+        unique_letters.insert(c);
+    }
+
+    int max = 0;
+    for(auto iti = unique_letters.begin(); iti != std::prev(unique_letters.end()); ++iti) {
+        for(auto itj = std::next(iti); itj != unique_letters.end(); ++itj) {
+            int result = sequence_length(input, *iti, *itj);
+            max = (result > max ? result : max);
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    int n;
+    std::string input;
+    std::cin >> n >> input;
+
+    std::cout << max_sequence_length(input) << std::endl;
+
+    return 0;
+}
