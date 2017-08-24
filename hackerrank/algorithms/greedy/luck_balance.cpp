76d2cb8b037938f33ec26eca35248cce1496ae94
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-24 16:18:22 +0300

    [HR][ALGO][GRDY] Luck Balance

---
+++ hackerrank/algorithms/greedy/luck_balance.cpp
@@ -0,0 +1,29 @@
+// https://www.hackerrank.com/challenges/luck-balance
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<int> l{};
+    for(int number, t; n-- > 0 && std::cin >> number >> t;) {
+        // yes, we can collect here (t == 0) directly into luck_balance,
+        // but it seems like we may skip it for current constraints
+        l.push_back((t == 0) ? number : -number);
+    }
+
+    std::sort(l.begin(), l.end());
+    for(int i = 0; i < k; ++i) {
+        l[i] = abs(l[i]);
+    }
+
+    long long luck_balance = 0;
+    std::for_each(l.cbegin(), l.cend(), [&luck_balance](int n) { luck_balance += n; });
+
+    std::cout << luck_balance << std::endl;
+
+    return 0;
+}
