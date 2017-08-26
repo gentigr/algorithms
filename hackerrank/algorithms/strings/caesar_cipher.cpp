94ff9d493e3b7073a4e71cc0c09265b83db6cf5a
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-26 15:22:44 +0300

    [HR][ALGO][STR] Caesar Cipher

---
+++ hackerrank/algorithms/strings/caesar_cipher.cpp
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/caesar-cipher-1
+#include <string>
+#include <iostream>
+
+// ASCII: [65, 90] == ['A', 'Z'] , [97, 122] == ['a', 'z']
+// 25 == 'Z' - 'A' == 'z' - 'a'
+char caesar_cipher(char c, int k)
+{
+    if (c >= 'A' && c <= 'Z') {
+        return ((c - 'A' + k) % ('Z' - 'A' + 1)) + 'A';
+    } else if (c >= 'a' && c <= 'z') {
+        return ((c - 'a' + k) % ('Z' - 'A' + 1)) + 'a';
+    }
+
+    return c;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::string s;
+    std::cin >> s;
+
+    int k;
+    std::cin >> k;
+
+    for(int i = 0; i < s.size(); ++i) {
+        s[i] = caesar_cipher(s[i], k);
+    }
+
+    std::cout << s << std::endl;
+
+    return 0;
+}
