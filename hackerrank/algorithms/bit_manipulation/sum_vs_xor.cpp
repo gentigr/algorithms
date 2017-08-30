eee8762b1e12184a05ae0b1e7cbff0258153ed35
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-08-30 14:06:54 +0300

    [HR][ALGO][BITM] Sum vs XOR

---
+++ hackerrank/algorithms/bit_manipulation/sum_vs_xor.cpp
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/sum-vs-xor
+#include <iostream>
+
+int main()
+{
+    unsigned long long n;
+    std::cin >> n;
+
+    unsigned long long count = 0;
+    for(; n != 0; count += (n % 2 == 0), n >>= 1);
+
+    std::cout << (1L << count) << std::endl;
+
+    return 0;
+}
