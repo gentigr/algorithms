e146a0a1bdd3b1b2dc08936bb421b59de77ee681
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-09-02 21:52:28 +0300

    [HR][ALGO][IMPL] Bon Appétit

---
+++ hackerrank/algorithms/implementation/bon_appetit.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/bon-appetit/problem
+#include <iostream>
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+
+    int cost = 0;
+    for(int number; n-- > 0 && std::cin >> number; cost += (k-- == 0 ? 0 : number));
+
+    int b;
+    std::cin >> b;
+    std::cout << (cost / 2 == b ? "Bon Appetit" : std::to_string(b - cost / 2)) << std::endl;
+
+    return 0;
+}
