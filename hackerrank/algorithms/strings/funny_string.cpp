78e04534abc57aef1aa691c8a8c6ec76ff30f55e
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-09-03 16:51:34 +0300

    [HR][ALGO][STR] Funny String

---
+++ hackerrank/algorithms/strings/funny_string.cpp
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/funny-string
+#include <iostream>
+
+int main()
+{
+    int t;
+    std::cin >> t;
+
+    for(std::string s; t-- > 0 && std::cin >> s;) {
+        bool valid = true;
+        for(int i = 1; i < s.size() / 2 + 1 && valid; i++) {
+            if (abs(s[i] - s[i - 1]) != abs(s[s.size() - i - 1] - s[s.size() - i])) {
+                valid = false;
+            }
+        }
+        std::cout << (valid ? "Funny" : "Not Funny") << std::endl;
+    }
+
+    return 0;
+}
+
