1b946df80dacab7d5aa858e4b2e8d4f0d8b22dfa
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-09-08 18:48:28 +0300

    [HR][ALGO][IMPL] The Hurdle Race

---
+++ hackerrank/algorithms/implementation/the_hurdle_race.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/the-hurdle-race
+#include <iostream>
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+
+    int max = 0;
+    for(int number; n-- > 0 && std::cin >> number;) {
+        max = number > max ? number : max;
+    }
+
+    std::cout << (k > max ? 0 : max - k) << std::endl;
+
+    return 0;
+}
