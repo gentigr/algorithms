9eeb200ff68276d99050926c1b5ae68fc0bdca22
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-09-09 22:00:34 +0300

    [HR][ALGO][IMPL] Designer PDF Viewer

---
+++ hackerrank/algorithms/implementation/designer_pdf_viewer.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/designer-pdf-viewer
+#include <array>
+#include <iostream>
+
+#define LETTERS_COUNT 26
+
+int main()
+{
+    std::array<int, LETTERS_COUNT> alphabet{};
+    for(int i = 0, height; i < LETTERS_COUNT && std::cin >> height; alphabet[i] = height, ++i);
+
+    std::string word;
+    std::cin >> word;
+
+    int max = 0;
+    for(char c : word) {
+        max = (alphabet[c - 'a'] > max) ? alphabet[c - 'a'] : max;
+    }
+
+    std::cout << (word.size() * max) << std::endl;
+
+    return 0;
+}
