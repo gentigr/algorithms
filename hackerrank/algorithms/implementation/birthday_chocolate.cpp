1f89adb0322127d6406d98c38c729a58871d9221
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-10-30 19:00:42 -0400

    [HR][ALGO][IMPL] Birthday Chocolate

---
+++ hackerrank/algorithms/implementation/birthday_chocolate.cpp
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/the-birthday-bar/problem
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    int count;
+    std::cin >> count;
+
+    std::vector<int> squares;
+    for (int n; count-- > 0 && std::cin >> n; squares.push_back(n));
+
+    int d, m;
+    std::cin >> d >> m;
+
+    int sum = 0, breaks_count = 0;
+    for (int i = 0; i < m && i < squares.size(); sum += squares[i], ++i);
+    breaks_count += (sum == d);
+    for (int i = 0, j = m; j < squares.size(); ++i, ++j) {
+        sum = sum - squares[i] + squares[j];
+        breaks_count += (sum == d);
+    }
+
+    std::cout << breaks_count << std::endl;
+
+    return 0;
+}
