4acea8f6c95514545122de90fac48b74c982fcdd
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-10-31 19:40:51 -0400

    [HR][ALGO][STR] Pangrams

---
+++ hackerrank/algorithms/strings/pangrams.cpp
@@ -0,0 +1,29 @@
+// https://www.hackerrank.com/challenges/pangrams/problem
+#include <array>
+#include <iostream>
+#include <algorithm>
+
+bool is_pangram(const std::string& sentence)
+{
+    const int ALPHABET_LENGTH = 25;
+    std::array<int, ALPHABET_LENGTH> letters{};
+
+    // there is should be check for ' ' inside this lambda
+    std::for_each(sentence.cbegin(), sentence.cend(), [&letters](char c) {
+        ++letters[c - ((c > 'Z')? 'a' : 'A')];
+    });
+
+    return std::none_of(letters.cbegin(), letters.cend(), [](int i) {
+        return i == 0;
+    });
+}
+
+int main()
+{
+    std::string s;
+    std::getline(std::cin, s);
+
+    std::cout << (is_pangram(s) ? "" : "not ") << "pangram" << std::endl;
+
+    return 0;
+}
