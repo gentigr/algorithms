e946970eb1c002df4d227afb2c22ed94998a5ef3
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-11-06 21:03:16 -0500

    [HR][ALGO][SORT] Counting Sort 1

---
+++ hackerrank/algorithms/sorting/counting_sort_1.cpp
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/countingsort1/problem
+#include <array>
+#include <iostream>
+
+#define MAX_ARRAY_SIZE 100
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::array<int, MAX_ARRAY_SIZE> counters{};
+    for(int num; n-- > 0 && std::cin >> num; ++counters[num]);
+
+    for(int num = 0; num < counters.size(); std::cout << counters[num++] << " ");
+    std::cout << std::endl;
+
+    return 0;
+}
