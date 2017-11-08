8ad848c9cbce76af718493be123aa383873975a6
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-11-07 19:42:35 -0500

    [HR][ALGO][SORT] Counting Sort 2

---
+++ hackerrank/algorithms/sorting/counting_sort_2.cpp
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/countingsort2/problem
+#include <array>
+#include <iostream>
+
+#define MAX_NUMBER_VALUE 100
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::array<int, MAX_NUMBER_VALUE> counters{};
+    for(int num; n-- > 0 && std::cin >> num; ++counters[num]);
+
+    for(int num = 0; num < counters.size(); ++num) {
+        for (; counters[num]-- > 0; std::cout << num << " ");
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
