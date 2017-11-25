67a0556d1f18eed65ecab471e450bc602497e9b1
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2017-11-25 04:15:40 -0500

    [HR][ALGO][SRCH] Pairs

---
+++ hackerrank/algorithms/search/pairs.cpp
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/pairs/problem
+#include <set>
+#include <queue>
+#include <iostream>
+
+int main()
+{
+    long int n, k;
+    std::cin >> n >> k;
+
+    std::set<long int> seq;
+    for(long int num; n-- > 0 && std::cin >> num; seq.insert(num));
+
+    long int count = 0;
+    std::queue<long int> accumulator{};
+    for(int item : seq) {
+        while(!accumulator.empty() && accumulator.front() <= item) {
+            count += (accumulator.front() == item);
+            accumulator.pop();
+        }
+        accumulator.push(item + k);
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
+}
+// NOTE: more conscious solution for this problem consists of using
+//       unordered_set for `seq` and than iterating over collection with
+//       count += seq.count(*it + k);
